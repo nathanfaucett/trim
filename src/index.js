12a149e1ce88c5c5b9d2a19ad536724fe1d2d518
nathanfaucett <nathanfaucett@gmail.com>
2015-01-21 10:42:03 -0600

    added trim left and right

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ var isNative = require("is_native"),
     isString = require("is_string");
 
 
-var nativeTrim = String.prototype.trim,
+var StringPrototype = String.prototype,
+
+    nativeTrim = StringPrototype.trim,
+    nativeTrimLeft = StringPrototype.trimLeft,
+    nativeTrimRight = StringPrototype.trimRight,
+
     reTrim = /^[\s\xA0]+|[\s\xA0]+$/g,
-    baseTrim;
+    reTrimLeft = /^[\s\xA0]+/g,
+    reTrimRight = /[\s\xA0]+$/g,
+
+    baseTrim, baseTrimLeft, baseTrimRight;
+
+
+module.exports = trim;
 
 
 if (isNative(nativeTrim)) {
@@ -17,7 +28,35 @@ if (isNative(nativeTrim)) {
     };
 }
 
+if (isNative(nativeTrimLeft)) {
+    baseTrimLeft = function baseTrimLeft(str) {
+        return nativeTrimLeft.call(str);
+    };
+} else {
+    baseTrimLeft = function baseTrimLeft(str) {
+        return str.replace(reTrimLeft, "");
+    };
+}
 
-module.exports = function trim(str) {
+if (isNative(nativeTrimRight)) {
+    baseTrimRight = function baseTrimRight(str) {
+        return nativeTrimRight.call(str);
+    };
+} else {
+    baseTrimRight = function baseTrimRight(str) {
+        return str.replace(reTrimRight, "");
+    };
+}
+
+
+function trim(str) {
     return baseTrim(isString(str) ? str : String(str));
+}
+
+trim.left = function trimLeft(str) {
+    return baseTrimLeft(isString(str) ? str : String(str));
+};
+
+trim.right = function trimRight(str) {
+    return baseTrimRight(isString(str) ? str : String(str));
 };
--- test/test.js
+++ test/test.js
@@ -7,3 +7,15 @@ describe("trim(str)", function() {
         assert.equal(trim(" \n hey \t\n"), "hey");
     });
 });
+
+describe("trim.left(str)", function() {
+    it("should trim left of string", function() {
+        assert.equal(trim.left(" \n hey \t\n"), "hey \t\n");
+    });
+});
+
+describe("trim.right(str)", function() {
+    it("should trim right of string", function() {
+        assert.equal(trim.right(" \n hey \t\n"), " \n hey");
+    });
+});
